bf06988d67a3dc607a82840f869af923454ac02f
Moshe Berman <moshberm@gmail.com>
2012-11-27 13:02:17 -0500

    Added the shul data container object.

---
+++ GoDavenImporter/GDShul.h
@@ -0,0 +1,16 @@
+//
+//  GDShul.h
+//  GoDavenImporter
+//
+//  Created by Moshe Berman on 11/27/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GDShul : NSObject
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *address;
+
+@end
---
+++ GoDavenImporter/GDShul.m
@@ -0,0 +1,13 @@
+//
+//  GDShul.m
+//  GoDavenImporter
+//
+//  Created by Moshe Berman on 11/27/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#import "GDShul.h"
+
+@implementation GDShul
+
+@end
