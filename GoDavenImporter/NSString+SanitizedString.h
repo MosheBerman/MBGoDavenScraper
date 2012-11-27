6135d76e55b8f466cd1fef293a8de1b47502f7cb
Moshe Berman <moshberm@gmail.com>
2012-11-27 13:19:53 -0500

    Added a category to clean up parsed strings.

---
+++ GoDavenImporter/NSString+SanitizedString.h
@@ -0,0 +1,15 @@
+//
+//  NSString+SanitizedString.h
+//
+//
+//  Created by Moshe Berman on 11/27/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (SanitizedString)
+
+- (NSString *) sanitizedString;
+
+@end
---
+++ GoDavenImporter/NSString+SanitizedString.m
@@ -0,0 +1,21 @@
+//
+//  NSString+SanitizedString.m
+//  GoDavenImporter
+//
+//  Created by Moshe Berman on 11/27/12.
+//  Copyright (c) 2012 Moshe Berman. All rights reserved.
+//
+
+#import "NSString+SanitizedString.h"
+
+@implementation NSString (SanitizedString)
+
+- (NSString *) sanitizedString{
+    return [[[[[self stringByDeletingOccurencesOfString:@"<span>"] stringByDeletingOccurencesOfString:@"</span>"] stringByDeletingOccurencesOfString:@"<br>"] stringByDeletingOccurencesOfString:@"<p>"] stringByDeletingOccurencesOfString:@"</p>"];
+    
+}
+
+- (NSString *) stringByDeletingOccurencesOfString:(NSString *)string{
+ return [self stringByReplacingOccurrencesOfString:string withString:@""];
+}
+@end
